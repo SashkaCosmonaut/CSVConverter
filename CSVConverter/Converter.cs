422a0a45693ee9a1c167864cb518cd2a668271c4
Sergey Nozdrenkov <nozdrenkov@gmail.com>
2015-06-24 17:24:38 +0300

    Empty project

---
+++ CSVConverter/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConversion
+{
+    public class Converter
+    {
+        public bool Import(String path)
+        {
+            return false;
+        }
+
+        virtual public bool Export(String path);
+    }
+}
