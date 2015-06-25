d6af7ee018fe581880ee41c88cf1e23199e5f955
Sergey Nozdrenkov <nozdrenkov@gmail.com>
2015-06-25 07:24:46 +0300

    Added XLSX converter

--- CSVConverter/XLSXConverter.cs
+++ CSVConverter/XLSXConverter.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Data;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using OfficeOpenXml;
 
 namespace DataConversion
 {
@@ -10,7 +14,29 @@ namespace DataConversion
     {
         public override bool Export(String path)
         {
-            return false;
+            try
+            {
+                Console.WriteLine("Export of .xlsx file...");
+                var timer = new Stopwatch();
+                timer.Start();
+                var file = new FileInfo(path);
+                using (var pck = new ExcelPackage(file))
+                {
+                    ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Sheet1");
+                    ws.Cells["A1"].LoadFromDataTable(dataTable, true);
+                    ws.Cells.AutoFitColumns();
+                    pck.Save();
+                }
+
+                timer.Stop();
+                Console.WriteLine("Export complete! Elapsed time: {0} ms", timer.Elapsed.Milliseconds);
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("Export failed! Please check, may be file with such name is already exist...");
+                return false;
+            }
         }
     }
 }
